b78e545fe7932c034e9e8191a300b2412b74d924
Minh Truong <truongminhnguyen242@gmail.com>
2026-09-21 18:26:07 +0700

    Render data

--- js/script.js
+++
@@ -1,8 +0,0 @@
-console.log("Bai tap async");
-
-// fetch => raw data (Response) -> object
-const temp = fetch("http://localhost:8000/users");
-
-temp
-.then((res) => res.json())  // res sang object cua js
-.then((d) => console.log(d));
